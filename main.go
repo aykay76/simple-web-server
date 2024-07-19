015e377b61b1a979f5d1bb985c887d3dcbe18df1
aykay76 <alan.kelly.london@gmail.com>
2024-07-19 15:36:09 +0200

    htmx example

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 // Given a request send it to the appropriate url
 func handleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
-	fmt.Println(req)
+	fmt.Println(req.URL.Path)
 
+	if req.URL.Path == "/index.html" {
+		res.Header().Add("Content-Type", "text/html")
+		content, err := os.ReadFile("index.html")
+		if err == nil {
+			res.Write(content)
+		} else {
+		}
+	} else if req.URL.Path == "/clicked" {
+		res.Header().Add("Content-Type", "text/html")
+		res.Write([]byte("Nice!"))
+	}
 }
 
 func main() {
